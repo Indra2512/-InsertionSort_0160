a587e459363e4db1026b3677817328beae75c249
Indra2512 <mononeon1225@gmail.com>
2025-03-24 16:21:16 +0700

    Menambahkan prosedur isertionsort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -32,5 +32,26 @@ void input()
     }
 }
 
+void insertionsort()
+{ // procedure insertionsort
 
+    int temp; // membuat variable data temporer atau penyimpanan sementara
+    int j, i; // membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+    { // step 1
+
+        temp = arr[i]; //step 2
+
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) // step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j--; // step 4b
+        }
+
+        arr[j + 1] = temp; // step 5
+    }
+}
 
